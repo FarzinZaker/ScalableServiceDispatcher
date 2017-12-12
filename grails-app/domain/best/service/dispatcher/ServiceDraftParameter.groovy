21dc49fbcafc43827674edc88ff1640cba5e14eb
srmo642002@gmail.com <srmo642002@gmail.com>
2017-12-12 09:39:58 +0330

    clob service draft parameter

--- grails-app/domain/best/service/dispatcher/ServiceDraftParameter.groovy
+++ grails-app/domain/best/service/dispatcher/ServiceDraftParameter.groovy
@@ -5,7 +5,9 @@ class ServiceDraftParameter {
     ServiceDraft draft
     ServiceParameter parameter
     String value
-
+    static mapping = {
+        value sqlType: 'clob'
+    }
     static constraints = {
         value nullable: true
     }
